1c5e9c1a67827c25e13734d901a4592fc883e78f
Virendrasingh <virendrasinghpawar13@gmail.com>
2025-05-19 11:12:45 +0530

    Update build script to use Python 3.9, remove Procfile, and adjust Vercel configuration for static file handling

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn hacker.wsgi:application 
--- hacker/wsgi.py
+++ hacker/wsgi.py
@@ -2,7 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hacker.settings')
-
 application = get_wsgi_application()
+# add this vercel variable
 app = application
