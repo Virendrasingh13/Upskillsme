41d5136477ff08a08b1ef591d373ffa5732f8d3c
Virendrasingh <161146998+Virendrasingh13@users.noreply.github.com>
2025-05-21 21:37:14 +0530

    Update settings.py

--- hacker/settings.py
+++ hacker/settings.py
@@ -76,6 +76,9 @@ if DEVELOPMENT_MODE is True:
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
+    db_from_env = dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    db_from_env["ENGINE"] = "django.db.backends.postgresql"  
+
     DATABASES = {
         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
